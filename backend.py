1b2d6b9f704c8ed546695974b4c3a19d0c59d2e7
lewyuejian <lewyuejian@163.com>
2020-09-19 04:30:35 +0800

    测试用例的API

--- backend.py
+++ backend.py
@@ -5,7 +5,7 @@
 @time: 2020/9/17 0017 20:37
 @desc:
 '''
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 
@@ -40,10 +40,39 @@ class TestCase(db.Model):
 
 class TestCaseApi(Resource):
     def get(self):
-        return {'hello': 'world'}
+
+        r = []
+        for t in TestCase.query.all():
+            res = {}
+            res['id'] = t.id
+            res['name'] = t.name
+            res['description'] = t.description
+            res['data'] = t.data
+            r.append(res)
+        return r
+
+
+    def post(self):
+        t = TestCase(
+            name=request.json['name'],
+            description=request.json['description'],
+            data=request.json['data']
+        )
+        db.session.add(t)
+        db.session.commit()
+        return {
+            'msg':'ok'
+        }
+
+    def put(self):
+        pass
+
+    def delete(self):
+        pass
 
 class LoginApi(Resource):
     def get(self):
+        User.query.all()
         return {'hello': 'world'}
 
 api.add_resource(TestCaseApi, '/testcase')
